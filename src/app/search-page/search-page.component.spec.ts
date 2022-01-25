0e76b7f4e1a38470c93345f6022abd14cc8405b2
Denys Stepanov <denys.stepanov.extern@atruvia.de>
2022-01-26 01:17:10 +0200

    feat: tp-0002 implementation process - part1

--- src/app/search-page/search-page.component.spec.ts
+++ src/app/search-page/search-page.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { SearchPageComponent } from './search-page.component';
 
@@ -9,6 +10,7 @@ describe('SearchFormComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
+            imports: [RouterTestingModule],
             declarations: [SearchPageComponent]
         })
         .compileComponents();
--- src/app/token-page/token-page.component.spec.ts
+++ src/app/token-page/token-page.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { TokenPageComponent } from './token-page.component';
+
 
 describe('TokenPageComponent', () => {
     let component: TokenPageComponent;
@@ -8,6 +10,7 @@ describe('TokenPageComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
+            imports: [RouterTestingModule],
             declarations: [TokenPageComponent]
         })
         .compileComponents();
